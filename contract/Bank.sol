032e529eb21fe9e688c8e3ba99fbea213775f7cf
NatielSanti <natielsanti@mail.ru>
2024-11-07 20:51:30 +0300

    init

---
+++ contract/Bank.sol
@@ -0,0 +1,28 @@
+// Указываем версию компилятора
+pragma solidity ^0.8.0;
+
+contract SimpleBank {
+    // Адрес владельца контракта
+    address public owner;
+
+    // Балансы пользователей
+    mapping(address => uint) public balances;
+
+    // Инициализируем контракт, устанавливая владельца
+    constructor() {
+        owner = msg.sender;
+    }
+
+    // Функция для внесения средств на контракт
+    function deposit() public payable {
+        require(msg.value > 0, "Нельзя внести 0");
+        balances[msg.sender] += msg.value;
+    }
+
+    // Функция для снятия средств
+    function withdraw(uint amount) public {
+        require(amount <= balances[msg.sender], "Недостаточно средств");
+        balances[msg.sender] -= amount;
+        payable(msg.sender).transfer(amount);
+    }
+}
